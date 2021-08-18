c8abe4780a16a03ef0a460898224debd8d5086b2
Gareth Macdonald <macdonald.gareth@gmail.com>
2021-08-18 11:57:13 +0200

    add click test to ShoppingBag tests

--- components/__tests__/ShoppingBag.test.tsx
+++ components/__tests__/ShoppingBag.test.tsx
@@ -1,12 +1,26 @@
 import React from 'react';
-import { render } from '../../utils/testUtils';
+import { render, fireEvent } from '../../utils/testUtils';
 import { ShoppingBag } from '../elements/ShoppingBag';
 
 describe('ShoppingBag', () => {
-  test('Number of items in cart is correct', async () => {
-    const mockShop = [{ quantity: 2 }];
-    const { container } = render(<ShoppingBag shoppingCart={mockShop} />);
+  test('Number of items in cart is rendered correctly', async () => {
+    const mockShopCart = [{ quantity: 2 }];
+    const { container } = render(<ShoppingBag shoppingCart={mockShopCart} />);
     const bagItemsCount = container.querySelector('p')?.innerHTML;
     expect(bagItemsCount).toBe('2');
+  });
+  test('Clicking care will setCartVisible to true', async () => {
+    const mockShopCart = [{ quantity: 2 }];
+    const setCartVisibleMock = jest.fn();
+    const { container } = render(
+      <ShoppingBag
+        shoppingCart={mockShopCart}
+        setCartVisible={setCartVisibleMock}
+      />
+    );
+    const bag = container.querySelector('div');
+    fireEvent.click(bag);
+    expect(setCartVisibleMock.mock.calls.length).toBe(1);
+    expect(setCartVisibleMock.mock.calls[0][0]).toBe(true);
   });
 });
